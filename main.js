0aafca3d87f3ef2f2dde2fed32797bf948af1b8e
Qinghao Cai <mikecqh@terpmail.umd.edu>
2023-04-06 23:15:16 -0400

    homepage and food page update

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
 const tabs = ['home', 'food', 'fitness', 'sleep'];
 
-function showTab (t) {
-    tabs.forEach(tab => {
-      document.getElementById(tab).classList.add('hidden');
-      document.getElementById(`open-${tab}`).classList.remove('active');
-    });
-    document.getElementById(t).classList.remove('hidden');
-    document.getElementById(`open-${t}`).classList.add('active');
+function formatTime (time) {
+  let [hh, mm] = time.split(':').map(v => Number(v));
+  if (hh > 12) {
+    return `${hh - 12}: ${mm} PM`;
   }
-  
-  tabs.forEach(t => {
-    document.getElementById(`open-${t}`).addEventListener('click', () => {
-      showTab(t);
-    });
+  return `${hh}: ${mm} AM`;
+}
+
+function findLatestMeal () {
+  let foodStore = localStorage.getItem('store');
+  if (foodStore) {
+    foodStore = JSON.parse(foodStore);
+    if (foodStore.list) {
+      foodStore.list.sort((a, b) => b.date - a.date);
+      for (let day of foodStore.list) {
+        if (day.meals) {
+          day.meals.sort((a, b) => b.Time.localeCompare(a.Time));
+          // console.log(day.meals);
+          for (let meal of day.meals) {
+            return meal;
+          }
+        }
+      }
+    }
+  }
+}
+
+function showTab (t) {
+  tabs.forEach(tab => {
+    document.getElementById(tab).classList.add('hidden');
+    document.getElementById(`open-${tab}`).classList.remove('active');
   });
-  
-  showTab(tabs[0]);
+  document.getElementById(t).classList.remove('hidden');
+  document.getElementById(`open-${t}`).classList.add('active');
+
+  if (t === 'home') {
+    let f = findLatestMeal();
+    if (f) {
+      document.getElementById('recentFood').textContent = `${formatTime(f.Time)} - ${f.Name}`
+    } else {
+      document.getElementById('recentFood').textContent = `No record.`
+    }
+  }
+}
+
+tabs.forEach(t => {
+  document.getElementById(`open-${t}`).addEventListener('click', () => {
+    showTab(t);
+  });
+});
+
+showTab(tabs[0]);
